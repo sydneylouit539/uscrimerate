a14f6cfd32a9e88953af8a949b6b5a9a44dd5c35
Ziqi7 <80850452+Ziqi7@users.noreply.github.com>
2021-04-20 15:13:48 -0400

    Update ziqi-readdata.R
    add more comments

--- ziqi-readdata.R
+++ ziqi-readdata.R
@@ -8,14 +8,14 @@ for (i in 1999:2019){
   if (i<=2004){
     crimerate <- sqldf("select * from crime where `Table 5` = 'Rate per 100,000 inhabitants'")
     if (i<=2002){
-      crimerate <- crimerate[-c(1:3,11,15)]
+      crimerate <- crimerate[-c(1:3,11,15)]#2001-2002
     }else{
       crimerate <- crimerate[c(2:11)]#2003-2004
     }
   }else if(i>=2013 & i<=2016){
-    crimerate = crime[which(crime[,3]=='Rate per 100,000 inhabitants'),c(4:6,8:14)]#2005-2012
+    crimerate = crime[which(crime[,3]=='Rate per 100,000 inhabitants'),c(4:6,8:14)]#2013-2016
   }else{
-    crimerate = crime[which(crime[,3]=='Rate per 100,000 inhabitants'),c(4:13)]
+    crimerate = crime[which(crime[,3]=='Rate per 100,000 inhabitants'),c(4:13)]#2005-2012,2017-2019
   }
   if (i<=2002){
     names(crimerate) = c("STATE","VIOLENTCRIME","PROPERTYCRIME","MURDER","RAPE","ROBBERY","AGGRAVATEDASSAULT","BURGLARY","LARCENYTHEFT","MOTORVEHICLETHEFT")
@@ -36,6 +36,7 @@ for (i in 1999:2019){
 #combine dataset
 c9902<-rbind(c1999,c2000,c2001,c2002)
 library(dplyr)
+#change order of columns of c9902
 c9902 <- c9902 %>% select(STATE,VIOLENTCRIME,MURDER,RAPE,ROBBERY,AGGRAVATEDASSAULT,PROPERTYCRIME,BURGLARY,LARCENYTHEFT,MOTORVEHICLETHEFT,YEAR)
 Total<-rbind(c9902,c2003,c2004,c2005,c2006,c2007,c2008,c2009,c2010,c2011,c2012,c2013,c2014,c2015,c2016,c2017,c2018,c2019)
 write.csv(x = Total,file = "crimerate.csv",row.names=F)
